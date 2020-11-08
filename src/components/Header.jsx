771faf03040abfac7c762ff97042c2460119b7fe
PRATIK <pratikchaubey05@gmail.com>
2020-11-08 16:57:27 +0530

    initial commit: project begins

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+function Header() {
+  return (
+    <header>
+      <h1><AssignmentIcon /> Parchi</h1>
+    </header>
+  );
+}
+
+export default Header;
