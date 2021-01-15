7edffa1b8f60608ef523aa181608bb3d07332465
PRATIK <pratikchaubey05@gmail.com>
2021-01-15 16:39:59 +0530

    1) Resolved a bug in the frontend

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,15 @@ dotenv.config();
 const app = express();
 
 
-app.get("/", (req, res)=>{
-    res.send("API is running") ;
-})
-
 const __dirname = path.resolve() ;
 if(process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "/frontend/build"))) ;
 
     app.get("*", (req, res)=> res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html")) )
+}else{
+    app.get("/", (req, res)=>{
+        res.send("API is running") ;
+    })
 }
 
 const PORT = process.env.PORT || 5000 ;
